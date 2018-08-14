632fbd4a0afa4ed281e0852ba99e1da7beb56fde
Jiliang.WANG <Jiliang.WANG@TLN003.telino.com>
2018-08-14 15:54:52 +0200

    Spring React Webflux

---
+++ ReactCore/src/main/java/come/telino/tries/react/TryReactCore.java
@@ -0,0 +1,23 @@
+package come.telino.tries.react;
+
+import java.time.Duration;
+
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+public class TryReactCore {
+
+	public static void main(String[] args) {
+//		List<Integer> elements = new ArrayList<>();
+		
+		ConnectableFlux<Object> publish = Flux.create(fluxSink -> {while(true) {
+			fluxSink.next(System.currentTimeMillis());
+			}}).sample(Duration.ofSeconds(2)).publish();
+		
+		publish.subscribe(System.out::println);        
+//		publish.subscribe(System.out::println);
+		
+		publish.connect();
+	}
+
+}
