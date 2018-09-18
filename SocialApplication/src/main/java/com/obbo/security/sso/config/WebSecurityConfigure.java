6ccb77eeb3666760bdfde2b11ab10cdee2c3eff2
Jiliang Wang <jiliang.wang@telino.com>
2018-09-18 15:29:00 +0200

    Customize OAuth2Client

--- SocialApplication/src/main/java/com/obbo/security/sso/config/WebSecurityConfigure.java
+++ SocialApplication/src/main/java/com/obbo/security/sso/config/WebSecurityConfigure.java
@@ -1,18 +1,39 @@
 package com.obbo.security.sso.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import javax.servlet.Filter;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+	private final OAuth2ClientContext oauth2ClientContext;
+
+	public WebSecurityConfigure(OAuth2ClientContext oauth2ClientContext) {
+		super();
+		this.oauth2ClientContext = oauth2ClientContext;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+			.formLogin().disable()
 			.antMatcher("/**")
 			.authorizeRequests()
 				.antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
@@ -21,7 +42,42 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 			.logout()
 				.logoutSuccessUrl("/").permitAll()
 			.and()
-			.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			.csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			.and()
+			.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 	}
-
+	
+	private Filter ssoFilter() {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/login/facebook");
+		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+		facebookFilter.setRestTemplate(facebookTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(),
+				facebook().getClientId());
+		tokenServices.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(tokenServices);
+		return facebookFilter;
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
+	}
+	
+	
+	@Bean
+	public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
 }
